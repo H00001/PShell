f5a66b73ac1aaad605df35781cf19ab609150d03
frank <frankhan@gunplan.top>
2020-01-25 16:36:35 +0800

    add directry to shell link

--- main.c
+++ main.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+  #include <sys/types.h>
+       #include <sys/wait.h>
+
+void register_env();
+void func_signal(int s);
 int main(){
+	register_env();
 	char buf[20];
+	char dir[40];
 	while (1){
+		char *d = getcwd(dir,40);
+		printf("%s> ",d);
 		buf[strlen(fgets(buf,20,stdin))-1] = 0;
 		pid_t p;
 		if((p=fork())==0){
 			execlp(buf,buf,NULL);
 		}
 		else{
-			waitpid(p,NULL,0);
+			int result;
+			waitpid(p,&result,0);
+			if (WIFEXITED(result)) {
+				printf("cc");
+			}else{
+				printf("cd");
+			}
 		}
 	}
 }
+
+void register_env(){
+	signal(SIGINT,func_signal);
+}
+
+void func_signal(int s){
+	
+}
+
