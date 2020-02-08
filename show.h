2628ce4b1e7c1a6bb85939a1dd7006d1115a1fdb
frank <frankhan@gunplan.top>
2020-02-08 20:47:23 +0800

    update color

--- show.h
+++ show.h
@@ -1,3 +1,3 @@
 #include <stdio.h>
-#define SHOWGREEN(DATA...) ( printf("\033[32,1m%s\033[0m>",(DATA)) )
-#define SHOWRED(DATA...) ( printf("\033[31,1m%s\033[0m>",(DATA)) )
+#define SHOWGREEN(DATA...) ( printf("\033[32;1m%s\033[0m>",(DATA)) )
+#define SHOWRED(DATA...) ( printf("\033[31;1m%s\033[0m>",(DATA)) )
